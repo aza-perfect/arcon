8ff4ba18c7d4a1526027468108a003267e43d6e8
aza-bi <amuhitdinov69@gmail.com>
2025-05-06 08:57:50 +0500

    Remove Heroku configuration

--- arkon/settings.py
+++ arkon/settings.py
@@ -14,23 +14,21 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j240!z&_0wl4zi*(z1q02#ztqb@)&0apna^efvs$9+dl&#*hc#'
+# SECRET_KEY = 'django-insecure-j240!z&_0wl4zi*(z1q02#ztqb@)&0apna^efvs$9+dl&#*hc#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 TESTING = "test" in sys.argv
@@ -86,7 +84,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'arkon.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -99,19 +96,17 @@ WSGI_APPLICATION = 'arkon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':   os.getenv('PG_NAME',   'arkon'),
-        'USER':   os.getenv('PG_USER',   'postgres'),
-        'PASSWORD': os.getenv('PG_PASS', '1234'),
-        'HOST':   os.getenv('PG_HOST',   'localhost'),
-        'PORT':   os.getenv('PG_PORT',   '5432'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'NAME': os.environ.get('POSTGRES_DB', 'db_name'),
+        'USER': os.environ.get('POSTGRES_USER', 'username'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
-
 # переопределение для Heroku (если установлена переменная DATABASE_URL)
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 if db_from_env:
     DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -131,7 +126,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -143,7 +137,6 @@ TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -166,7 +159,7 @@ INTERNAL_IPS = [
 
 # NPM_BIN_PATH = r"C:/Program Files/nodejs/npm.cmd"
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
-LOGIN_URL          = 'production:login'
+LOGIN_URL = 'production:login'
 LOGIN_REDIRECT_URL = '/dashboard/'  # увидим ниже
 LOGOUT_REDIRECT_URL = LOGIN_URL
 # STATICFILES_DIRS = [BASE_DIR / "theme" / "static"]
